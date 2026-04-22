b5768f75360945024bbc8eeec13b0bb92bfc3f46
MeFlipanLosLimones <speedlimon34@gmail.com>
2026-04-21 20:09:25 -0600

    correeeee

---
+++ Skeletons/Assets/Scripts/SettingManager.cs
@@ -0,0 +1,39 @@
+using System;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+namespace Alejandro
+{
+    public class SettingManager : MonoBehaviour
+    {
+        [ReadOnly, SerializeField]private float _sfx = 0f;
+        [ReadOnly, SerializeField]private float _music = 0f;
+        
+        public static SettingManager instance{get;private set;}
+
+        public void Awake()
+        {
+            if (instance != null && instance != this)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+
+            instance = this;
+        }
+
+        public void SetSFXVolume(float value)
+        {
+            _sfx = value;
+            Debug.Log(_sfx);
+        }
+
+        public void SetMusicVolume(float value)
+        {
+            _music = value;
+        }
+        
+    }
+}
--- Skeletons/Assets/Scripts/SettingsUI.cs
+++ Skeletons/Assets/Scripts/SettingsUI.cs
@@ -1,7 +1,27 @@
+using UnityEngine.UI;
+
+
 namespace Alejandro
 {
     public class SettingsUI : UIWindow
     {
-        
+        public Slider sfxVolume;
+        public Slider musicVolume;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            sfxVolume.onValueChanged.AddListener(ChangeVolumeSFX);
+        }
+
+        private void ChangeVolumeSFX(float value)
+        {
+            SettingManager.instance.SetSFXVolume(value);
+        }
+
+        private void ChangeVolumeMusic(float value)
+        {
+            
+        }
     }
 }
