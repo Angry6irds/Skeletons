6402cdad0a5c594d32aae7946eb99e4a0db3df5a
MeFlipanLosLimones <speedlimon34@gmail.com>
2026-04-28 20:51:38 -0600

    .

--- Skeletons/Assets/Scripts/InventoryManager.cs
+++ Skeletons/Assets/Scripts/InventoryManager.cs
@@ -11,12 +11,14 @@ using UnityEngine;
     public class RuntimeItem
     {
         public string name;
+        public ItemType itemType;
         public Sprite sprite;
         
-        public  RuntimeItem(string name, Sprite sprite)
+        public  RuntimeItem(string name, Sprite sprite, ItemType itemType)
         {
             this.name = name;
             this.sprite = sprite;
+            this.itemType = itemType;
         }
     }
     public class InventoryManager : MonoBehaviour
@@ -35,13 +37,18 @@ using UnityEngine;
             Instance = this;
         }
 
+        void Start()
+        {
+            LoadInvetory();
+        }
+
         public void CreateItem(ItemType item)
         {
             foreach (var Item in  itemDatas )
             {
                 if (Item.itemType == item)
                 {
-                    RuntimeItem runtimeItem = new RuntimeItem(Item.name, Item.Icon);
+                    RuntimeItem runtimeItem = new RuntimeItem(Item.name, Item.Icon, Item.itemType);
                     items.Add(runtimeItem);
                 }
             }
@@ -51,6 +58,13 @@ using UnityEngine;
         {
             CreateItem(ItemType.axe);
             Debug.Log("CreateItemTest:" + items.Count);
+        }
+
+        [Button]
+        public void CreateItemTest2()
+        {
+            CreateItem(ItemType.sword);
+            Debug.Log("CreateItemTest2:" + items.Count);
         }
         
         [Button]
@@ -62,6 +76,24 @@ using UnityEngine;
             string path = Application.persistentDataPath + "/inventory.json";
             System.IO.File.WriteAllText(path, json);
             Debug.Log(path);
+            
         }
+        
+        public void LoadInvetory()
+        {
+            string path = Application.persistentDataPath + "/inventory.json";
+            if(!System.IO.File.Exists(path))
+            {
+                Debug.LogError("no inventory file found:"  + path);
+                return;
+            }
+            string json = System.IO.File.ReadAllText(path);
+            RuntimeItem[] loadItems = JsonHelper.FromJson<RuntimeItem>(json);
+            
+            items.Clear();
+            items.AddRange(loadItems);
+            
+        }
+        
     }
     
